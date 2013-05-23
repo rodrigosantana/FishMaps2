1910e4932f99dd2ade1bb899b694ef84f853b856
Fernando Mayer <fernandomayer@gmail.com>
2013-05-22 23:08:33 -0300

    first version of a function, defined with polygons

---
+++ R/levelmap.R
@@ -0,0 +1,49 @@
+levelmap <- function(x, data, xlim, ylim, lev, jump,
+                     database = "world", ...){
+    ## Base de dados para os mapas
+    mm <- maps::map(database = database, plot = FALSE, fill = TRUE)
+    ## Define os ranges do mapa, e os labels para colocar nos graficos
+    labsx <- seq(min(xlim), max(xlim), jump)
+    labsxc <- as.character(labsx)
+    labsxc[seq(2, length(labsxc), 2)] <- ""
+    labsy <- seq(min(ylim), max(ylim), jump)
+    labsyc <- as.character(labsy)
+    labsyc[seq(2, length(labsyc), 2)] <- ""
+    ## Define as classes
+    ## obj <- paste(all.vars(match.call())[4], all.vars(x)[1], sep = "$")
+    ## lev <- pretty(eval(obj), n)
+    ## Função para plotar valores zero
+    panel.zero.points <- function(x, y, z, subscripts, ...){
+        require("grid", quietly = TRUE)
+        x <- as.numeric(x)[subscripts]
+        y <- as.numeric(y)[subscripts]
+        z <- as.numeric(z)[subscripts]
+        for(i in seq(along = z)){
+            if(z[i] == 0L){
+                grid.points(x = x[i], y = y[i], pch = 3,
+                            size = unit(1, "native"))
+            } else{
+                grid.points(x = x[i], y = y[i], pch = "")
+            }
+        }
+    }
+    ## levelmap com levelplot
+    levelplot(x, data, ..., mm = mm, , aspect = "iso",
+              as.table = TRUE, xlim = xlim, ylim = ylim,
+              xlab = expression(paste("Longitude ", "(", degree, ")")),
+              ylab = expression(paste("Latitude ", "(", degree, ")")),
+              scales = list(x = list(at = labsx, labels = labsxc),
+                  y = list(at = labsy, labels = labsyc)),
+              strip = strip.custom(bg = "lightgrey"),
+              at = lev, colorkey = list(space="top"),
+              col.regions = grey.colors(length(lev)-1, start = 0.7, end = 0.1),
+              par.settings = list(layout.heights =
+                  list(top.padding = 0, bottom.padding = 0)),
+              panel = function(x, y, z, mm, ...){
+                  panel.levelplot(x, y, z, ...)
+                  panel.grid(h = -length(labsx), v = -length(labsy), ...)
+                  #panel.lines(mm$x, mm$y, col = "black")
+                  panel.polygon(mm$x, mm$y, border = "black", col = "lightgrey")
+                  panel.zero.points(x, y, z, ...)
+              })
+}
