3ffc60f4d982294a2c2e26b9330bef4fb08370cd
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2013-06-12 20:57:38 -0300

    Inseridos panel.na.points e batimetria do pacote Marelac

--- R/levelmap.R
+++ R/levelmap.R
@@ -1,8 +1,16 @@
 levelmap <- function(x, data, xlim, ylim, lev, jump,
                      key.space = "right",
-                     database = "world", ...){
+                     database = "worldHires",
+                     bathymetry = list(bb = TRUE, nlvl), ...){
     ## Base de dados para os mapas
     mm <- maps::map(database = database, plot = FALSE, fill = TRUE)
+    ## Base de dados de batimetrias - lembrando que é requisito do
+    ## FishMaps2 o pacote marelac
+    if(bathymetry$bb == TRUE) {
+        data(Bathymetry)
+        prof <- contourLines(Bathymetry$x, Bathymetry$y, Bathymetry$z,
+                     nlevels = bathymetry$nlvl)
+    }
     ## Define os ranges do mapa, e os labels para colocar nos graficos
     labsx <- seq(min(xlim), max(xlim), jump)
     labsxc <- as.character(labsx)
@@ -28,8 +36,24 @@ levelmap <- function(x, data, xlim, ylim, lev, jump,
             }
         }
     }
+    ## Função para plotar NA's
+    panel.na.points <- function(x, y, z, subscripts, ...){
+        require("grid", quietly = TRUE)
+        x <- as.numeric(x)[subscripts]
+        y <- as.numeric(y)[subscripts]
+        z <- as.numeric(z)[subscripts]
+        for(i in seq(along = z)){
+            if(is.na(z[i])) {
+                grid.points(x = x[i], y = y[i], pch = "-")
+            } else if(z[i] == 0L) {
+                grid.points(x = x[i], y = y[i], pch = "+")
+            } else {
+                grid.points(x = x[i], y = y[i], pch = "")
+            }
+        }
+    }
     ## levelmap com levelplot
-    levelplot(x, data, ..., mm = mm, , aspect = "iso",
+    levelplot(x, data, ..., mm = mm, prof = prof, aspect = "iso",
               as.table = TRUE, xlim = xlim, ylim = ylim,
               xlab = expression(paste("Longitude ", "(", degree, ")")),
               ylab = expression(paste("Latitude ", "(", degree, ")")),
@@ -41,11 +65,14 @@ levelmap <- function(x, data, xlim, ylim, lev, jump,
                   start = 0.7, end = 0.1),
               par.settings = list(layout.heights = list(top.padding = 0,
                                       bottom.padding = 0)),
-              panel = function(x, y, z, mm, ...){
+              panel = function(x, y, z, mm, prof, ...){
                   panel.levelplot(x, y, z, ...)
                   panel.grid(h = -length(labsx), v = -length(labsy), ...)
                   #panel.lines(mm$x, mm$y, col = "black")
-                  panel.polygon(mm$x, mm$y, border = "black", col = "snow")
-                  panel.zero.points(x, y, z, ...)
+                  lapply(prof, llines, col = "blue")
+                  panel.polygon(mm$x, mm$y, border = "black", col =
+                     "snow")
+                  #panel.zero.points(x, y, z, ...)
+                  panel.na.points(x, y, z, ...)
               })
 }
