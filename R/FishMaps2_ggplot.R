244bc48a35a3d9f55022cc76086ba6049c724e7a
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2014-01-08 01:46:01 -0200

    Reproducible example of FishMaps2 with ggplot

--- R/FishMaps2_ggplot.R
+++ R/FishMaps2_ggplot.R
@@ -5,18 +5,18 @@ dados <- read.table("../data/mapa.bum.csv", sep = ",", dec = ".", header = TRUE)
 ## Exclude trash column from data
 dados <- dados[,2:5]
 ## Plot map facet by Quarter without isobaths
-levelmap(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = '~Quarter', 
+levelmap_ggplot(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = '~Quarter', 
          basemap = 'world', xlim = c(-120,120), ylim = c(50, -90), 
          col.grd = c('gray', 'red'), bathymetry = FALSE)
 ## Plot map without segregation and isobaths         
-levelmap(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = NULL, 
+levelmap_ggplot(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = NULL, 
          basemap = 'world', xlim = c(-120,120), ylim = c(50, -90), 
          col.grd = c('gray', 'red'), bathymetry = FALSE)         
 ## Plot map segregated by Quarter with isobaths
-levelmap(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = '~Quarter', 
+levelmap_ggplot(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = '~Quarter', 
          basemap = 'world', xlim = c(-120,120), ylim = c(50, -90), 
          col.grd = c('gray', 'red'), bathymetry = TRUE)
 ## Plot map without segregation and with isobaths         
-levelmap(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = NULL, 
+levelmap_ggplot(dat = dados, x = 'Lon3', y = 'Lat3', fill = 'x', facet = NULL, 
          basemap = 'world', xlim = c(-120,120), ylim = c(50, -90), 
          col.grd = c('gray', 'red'), bathymetry = TRUE)
