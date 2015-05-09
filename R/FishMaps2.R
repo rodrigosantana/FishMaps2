bb17145c4c58192b6e04593f18e2e225b5465032
Fernando Mayer <fernandomayer@gmail.com>
2015-05-08 22:57:24 -0300

    creates a package document
    this file documents the hole package in general.
    The datsets available in the data/ directory must be documented
    in this file following the same structure.

---
+++ R/FishMaps2.R
@@ -0,0 +1,8 @@
+#' @title Proportional symbol mapping for fishery data in batch mode
+#'
+#' @description Proportional symbol mapping for fishery data in batch mode (ggplot2 version)
+#'
+#' @docType package
+#' @name FishMaps2
+NULL
+
