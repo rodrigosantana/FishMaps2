70fb232be809111d88bf4d80206b92276d8dfa2e
Fernando Mayer <fernandomayer@gmail.com>
2015-05-12 17:17:44 -0300

    start converting csv to rda

--- playground/test_script.R
+++ playground/test_script.R
@@ -1,5 +1,14 @@
-##----------------------------------------------------------------------
+##======================================================================
 ## Convertendo os arquivos csv em rda para poder colocar no pacote
+dir("../data")
+dat.bum <- read.table("../data/mapa.bum.csv", sep = ",", dec = ".", header = TRUE)
+str(dat.bum)
+dat.bum <- dat.bum[,-1]
+names(dat.bum) <- c("Quarter", "Lat", "Lon", "BUM")
+str(dat.bum)
+save("dat.bum", file = "../data/dat.bum.rda")
+##======================================================================
+
 
 ## Loading function
 source("../R/fishmap.R")
