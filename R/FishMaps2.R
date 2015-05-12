294a9f1ec065a77fba412ca07edefc1524fbff29
Fernando Mayer <fernandomayer@gmail.com>
2015-05-12 17:17:03 -0300

    documenting dat.bum dataset

--- R/FishMaps2.R
+++ R/FishMaps2.R
@@ -6,3 +6,22 @@
 #' @name FishMaps2
 NULL
 
+#' @title Blue marlin capture
+#' @name dat.bum
+#'
+#' @description Blue marlin capture by the long-line fleet in Atlantic
+#' Ocean
+#'
+#' @format A data frame with 496 rows and 4 variables:
+#' \itemize{
+#'   \item Quarter: quarter of the year
+#'   \item Lat: latitude centered in the middle of 5x5 squares
+#'   \item Lon: longitude centered in the middle of 5x5 squares
+#'   \item BUM: blue marlin capture
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @usage data(dat.bum)
+#' @source Based in ICCAT public databases
+NULL
